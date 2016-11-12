b5e0b3f1eea79132323d0c63cf40b665d3e2dc84
Alexander Malev <yttrium@somedev.ru>
2016-11-13 02:32:40 +0300

    base queue

---
+++ aioworkers/queue/base.py
@@ -0,0 +1,29 @@
+import asyncio
+
+from aioworkers.core.base import AbstractEntity
+
+
+class AbstractReader(AbstractEntity):
+    async def get(self):
+        raise NotImplementedError()
+
+
+class AbstractWriter(AbstractEntity):
+    async def put(self, value):
+        raise NotImplementedError()
+
+
+class AbstractQueue(AbstractReader, AbstractWriter):
+    pass
+
+
+class Queue(asyncio.Queue, AbstractQueue):
+    def __init__(self, config, *, context=None, loop=None):
+        AbstractQueue.__init__(self, config, context=context, loop=loop)
+        asyncio.Queue.__init__(self, maxsize=config.maxsize, loop=loop)
+
+
+class PriorityQueue(asyncio.PriorityQueue,  AbstractQueue):
+    def __init__(self, config, *, context=None, loop=None):
+        AbstractQueue.__init__(self, config, context=context, loop=loop)
+        asyncio.Queue.__init__(self, maxsize=config.maxsize, loop=loop)
