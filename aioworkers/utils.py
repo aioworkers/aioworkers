544c8ff7b76ab79f021933f3242858a4403acbed
Alexander Malev <yttrium@somedev.ru>
2016-11-17 00:31:25 +0300

    fix import_name

--- aioworkers/utils.py
+++ aioworkers/utils.py
@@ -2,6 +2,6 @@ import importlib
 
 
 def import_name(stref: str):
-    package, name = stref.rsplit(maxsplit=1)
+    package, name = stref.rsplit('.', maxsplit=1)
     module = importlib.import_module(package)
     return getattr(module, name)
