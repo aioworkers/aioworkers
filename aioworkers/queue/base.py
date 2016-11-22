bce64349462e930f4a5d32c12ebb89387e8e18f6
Alexander Malev <yttrium@somedev.ru>
2016-11-23 00:58:39 +0300

    fix pep8

--- aioworkers/queue/base.py
+++ aioworkers/queue/base.py
@@ -23,7 +23,7 @@ class Queue(asyncio.Queue, AbstractQueue):
         asyncio.Queue.__init__(self, maxsize=config.maxsize, loop=loop)
 
 
-class PriorityQueue(asyncio.PriorityQueue,  AbstractQueue):
+class PriorityQueue(asyncio.PriorityQueue, AbstractQueue):
     def __init__(self, config, *, context=None, loop=None):
         AbstractQueue.__init__(self, config, context=context, loop=loop)
         asyncio.Queue.__init__(self, maxsize=config.maxsize, loop=loop)
