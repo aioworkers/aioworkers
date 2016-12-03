9986f61cba5b54aa6d0c5f3e17a1e887702ec750
Alexander Malev <yttrium@somedev.ru>
2016-12-04 01:39:48 +0300

    FileSystemStorage based on FormattedEntity

--- aioworkers/storage/filesystem.py
+++ aioworkers/storage/filesystem.py
@@ -1,28 +1,28 @@
 import hashlib
 import os
 
+from ..core.formatter import FormattedEntity
 from . import base
 from .. import utils
 
 
-class FileSystemStorage(base.AbstractStorage):
+class FileSystemStorage(FormattedEntity, base.AbstractStorage):
     @property
     def executor(self):
-        if self._config.executor:
-            return self._context[self._config.executor]
+        return self._context[self._config.get('executor')]
 
     def _write(self, key, value):
         d = os.path.dirname(key)
         if not os.path.exists(d):
             os.makedirs(d)
         with open(key, 'wb') as f:
-            f.write(value)
+            f.write(self.encode(value))
 
     def _read(self, key):
         if not os.path.exists(key):
             return
         with open(key, 'rb') as f:
-            return f.read()
+            return self.decode(f.read())
 
     def _make_key(self, key):
         if os.path.isabs(key):
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,8 +1,12 @@
 import asyncio
 import tempfile
 
+import pytest
+
 from aioworkers.core.config import MergeDict
-from aioworkers.storage.filesystem import HashFileSystemStorage
+from aioworkers.core.context import Context
+from aioworkers.storage.filesystem import \
+    HashFileSystemStorage, FileSystemStorage
 
 
 async def test_set_get(loop):
@@ -14,8 +18,33 @@ async def test_set_get(loop):
             path=d,
             executor=None,
         )
-        storage = HashFileSystemStorage(config, loop=loop)
+        context = Context({}, loop=loop)
+        storage = FileSystemStorage(config, context=context, loop=loop)
+        await storage.init()
+
+        with pytest.raises(ValueError):
+            await storage.set('/abs/path', None)
+
+        assert not await storage.get('empty value')
+
         await storage.set(key, data)
         d = await asyncio.gather(storage.get(key), storage.get(key), loop=loop)
         for j in d:
             assert data == j
+
+
+async def test_pickle(loop):
+    key = '4423'
+    data = {'f': 3}
+    with tempfile.TemporaryDirectory() as d:
+        config = MergeDict(
+            name='',
+            path=d,
+            format='pickle',
+            executor=None,
+        )
+        context = Context({}, loop=loop)
+        storage = HashFileSystemStorage(config, context=context, loop=loop)
+        await storage.init()
+        await storage.set(key, data)
+        assert data == await storage.get(key)
