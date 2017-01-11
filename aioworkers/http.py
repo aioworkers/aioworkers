f6f2e0fa9ecb687f18afc4f71ee6136ac86b3efb
Alexander Malev <yttrium@somedev.ru>
2017-01-12 00:36:48 +0300

    link to context in app

--- aioworkers/http.py
+++ aioworkers/http.py
@@ -6,11 +6,12 @@ from .app import BaseApplication
 
 
 class Application(BaseApplication, web.Application):
-    def __init__(self, *, config, **kwargs):
+    def __init__(self, *, config, context, **kwargs):
         if kwargs.get('loop') is None:
             kwargs['loop'] = asyncio.get_event_loop()
         web.Application.__init__(self, **kwargs)
-        BaseApplication.__init__(self, config=config, **kwargs)
+        BaseApplication.__init__(
+            self, config=config, context=context, **kwargs)
 
     def run_forever(self, **kwargs):
         web.run_app(self, **kwargs)
