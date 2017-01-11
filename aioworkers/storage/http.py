5b64a52db63e9ffae25ad3cc1651983c4d5ccad6
Alexander Malev <yttrium@somedev.ru>
2017-01-12 00:46:27 +0300

    http storages

--- aioworkers/storage/http.py
+++ aioworkers/storage/http.py
@@ -1,4 +1,7 @@
 import asyncio
+import json
+import logging
+from collections import Mapping
 
 from aiohttp import client
 from yarl import URL
@@ -6,39 +9,100 @@ from yarl import URL
 from . import base
 
 
-class Storage(base.AbstractStorageReadOnly):
-    """
+class RoStorage(base.AbstractStorageReadOnly):
+    """ ReadOnly storage over http GET
     config:
         semaphore: int
         allow_hosts: list
         prefix: url prefix
+        template: url template
         format: [json|str|bytes]
     """
     async def init(self):
         self._prefix = self.config.get('prefix')
+        self._template = self.config.get('template')
         if self._prefix:
             self._prefix = URL(self._prefix)
         self._semaphore = asyncio.Semaphore(
-            self.config.semaphore, loop=self.loop)
+            self.config.get('semaphore', 20), loop=self.loop)
         self._allow_hosts = self.config.get('allow_hosts')
         self._format = self.config.get('format', 'json')
+        self.session = client.ClientSession(loop=self.loop)
 
-    async def get(self, key):
+    async def stop(self):
+        self.session.close()
+
+    def _make_url(self, key):
         if self._prefix:
             url = self._prefix / key
+        elif self._template and isinstance(key, Mapping):
+            url = URL(self._template.format_map(key))
+        elif self._template and isinstance(key, (list, tuple)):
+            url = URL(self._template.format(*key))
+        elif self._template:
+            url = URL(self._template.format(key))
         elif isinstance(key, str):
             url = URL(key)
         else:
             url = key
         if self._allow_hosts and url.host not in self._allow_hosts:
             raise KeyError(key)
+        return url
+
+    async def get(self, key):
+        url = self._make_url(key)
         async with self._semaphore:
-            with client.ClientSession(loop=self.loop) as session:
-                async with session.get(url) as response:
-                    if self._format in ('str', 'bytes'):
-                        data = await response.read()
-                    else:
-                        return await response.json()
+            async with self.session.get(url) as response:
+                logger = self.context.logger
+                if response.status == 404:
+                    return
+                elif response.status >= 400:
+                    if logger.getEffectiveLevel() == logging.DEBUG:
+                        logger.debug(
+                            'HttpStorage request to %s '
+                            'returned code %s:\n%s' % (
+                                url, response.status,
+                                (await response.read()).decode()))
+                    return
+                elif self._format in ('str', 'bytes'):
+                    data = await response.read()
+                else:
+                    return await response.json()
         if self._format == 'str':
             return data.decode()
         return data
+
+
+class Storage(RoStorage, base.AbstractStorageWriteOnly):
+    """ RW storage over http
+    config:
+        semaphore: int
+        allow_hosts: list
+        prefix: url prefix
+        template: url template
+        format: [json|str|bytes]
+        set: [post|put|patch]
+    """
+
+    @property
+    def method_set(self):
+        return getattr(self.session, self.config.get('set', 'post'))
+
+    async def set(self, key, value):
+        url = self._make_url(key)
+        if self._format == 'json':
+            data = json.dumps(value)
+            headers = {'content-type': 'application/json'}
+        else:
+            data = value
+            headers = {}
+        async with self._semaphore:
+            async with self.method_set(
+                    url, data=data, headers=headers) as response:
+                logger = self.context.logger
+                if logger.getEffectiveLevel() == logging.DEBUG:
+                    logger.debug(
+                        'HttpStorage request to %s returned code %s:\n%s' % (
+                            url, response.status,
+                            (await response.read()).decode()))
+                assert response.status <= 400, response.status
--- tests/test_storage_http.py
+++ tests/test_storage_http.py
@@ -1,4 +1,7 @@
+import pytest
+
 from aioworkers.core.config import MergeDict
+from aioworkers.core.context import Context
 from aioworkers.storage.http import Storage
 
 
@@ -10,6 +13,10 @@ async def test_set_get(loop):
         semaphore=1,
         format='str',
     )
-    storage = Storage(config, loop=loop)
+    context = Context(config=config, loop=loop)
+    await context.init()
+    storage = Storage(config, context=context, loop=loop)
     await storage.init()
     assert data in await storage.get(key)
+    with pytest.raises(AssertionError):
+        await storage.set(key, data)
