6357256bb52efeec57eece367572651ab3ff107a
Alexander Malev <yttrium@somedev.ru>
2017-01-12 00:41:33 +0300

    move & copy in filesystem storage

--- aioworkers/storage/filesystem.py
+++ aioworkers/storage/filesystem.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import shutil
 
 from ..core.formatter import FormattedEntity
 from . import base
@@ -13,10 +14,17 @@ class FileSystemStorage(FormattedEntity, base.AbstractStorage):
 
     def _write(self, key, value):
         d = os.path.dirname(key)
-        if not os.path.exists(d):
+        if os.path.exists(d):
+            pass
+        elif value is None:
+            return
+        else:
             os.makedirs(d)
-        with open(key, 'wb') as f:
-            f.write(self.encode(value))
+        if value is None:
+            os.remove(key)
+        else:
+            with open(key, 'wb') as f:
+                f.write(self.encode(value))
 
     def _read(self, key):
         if not os.path.exists(key):
@@ -40,11 +48,43 @@ class FileSystemStorage(FormattedEntity, base.AbstractStorage):
         return await self.loop.run_in_executor(
             self.executor, self._read, k)
 
+    def _copy(self, key_source, storage_dest, key_dest, copy_func):
+        s = self._make_key(key_source)
+        d = storage_dest._make_key(key_dest)
+        if os.path.exists(s):
+            copy_func(s, d)
+        elif not os.path.exists(d):
+            return False
+        else:
+            os.remove(d)
+            return True
 
-class HashFileSystemStorage(FileSystemStorage):
+    def copy(self, key_source, storage_dest, key_dest):
+        if isinstance(storage_dest, FileSystemStorage):
+            return self.loop.run_in_executor(
+                self.executor, self._copy, key_source,
+                storage_dest, key_dest, shutil.copy)
+        return super().copy(key_source, storage_dest, key_dest)
+
+    def move(self, key_source, storage_dest, key_dest):
+        if isinstance(storage_dest, FileSystemStorage):
+            return self.loop.run_in_executor(
+                self.executor, self._copy, key_source,
+                storage_dest, key_dest, shutil.move)
+        return super().move(key_source, storage_dest, key_dest)
+
+
+class NestedFileSystemStorage(FileSystemStorage):
+    def _nested(self, key):
+        return os.path.join(key[:2], key[2:4], key)
+
+    def _make_key(self, key):
+        return super()._make_key(self._nested(key))
+
+
+class HashFileSystemStorage(NestedFileSystemStorage):
     def _make_key(self, key):
         hash = hashlib.md5()
         hash.update(key.encode())
         d = hash.hexdigest()
-        k = os.path.join(d[:2], d[2:4], d)
-        return super()._make_key(k)
+        return super()._make_key(d)
