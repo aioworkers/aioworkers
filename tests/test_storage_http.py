f484c530be0c570df7f706897b22172fe6d2787d
Alexander Malev <yttrium@somedev.ru>
2017-02-22 03:29:11 +0300

    http storage tests++

--- tests/test_storage_http.py
+++ tests/test_storage_http.py
@@ -1,4 +1,5 @@
 import pytest
+from yarl import URL
 
 from aioworkers.core.config import MergeDict
 from aioworkers.core.context import Context
@@ -7,17 +8,32 @@ from aioworkers.storage.http import Storage
 
 
 async def test_set_get(loop):
-    key = 'https://github.com/aamalev/aioworkers'
-    data = 'aioworkers'
+    data = 'Python'
     config = MergeDict(
         name='',
+        prefix='https://api.github.com/',
         semaphore=1,
-        format='str',
+        format='json',
     )
     context = Context(config=config, loop=loop)
     await context.init()
     storage = Storage(config, context=context, loop=loop)
     await storage.init()
-    assert data in await storage.get(key)
+    assert data in await storage.get('repos/aamalev/aioworkers/languages')
     with pytest.raises(StorageError):
-        await storage.set(key, data)
+        await storage.set('user/repos', data)
+    await storage.stop()
+
+
+async def test_format(loop):
+    config = MergeDict(
+        name='',
+        semaphore=1,
+        format='bytes',
+    )
+    context = Context(config=config, loop=loop)
+    await context.init()
+    storage = Storage(config, context=context, loop=loop)
+    await storage.init()
+    assert isinstance(storage.raw_key('test'), URL)
+    await storage.stop()
