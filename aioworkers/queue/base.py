1eb6a53d5cbd6d4b95848622b3f5df5dae52be21
Alexander Malev <yttrium@somedev.ru>
2017-03-10 03:02:04 +0300

    ScoreQueueMixin

--- aioworkers/queue/base.py
+++ aioworkers/queue/base.py
@@ -1,14 +1,18 @@
 import asyncio
+from abc import abstractmethod
 
-from aioworkers.core.base import AbstractEntity
+from ..core.base import AbstractEntity
+from ..utils import import_name
 
 
 class AbstractReader(AbstractEntity):
+    @abstractmethod  # pragma: no cover
     async def get(self):
         raise NotImplementedError()
 
 
 class AbstractWriter(AbstractEntity):
+    @abstractmethod  # pragma: no cover
     async def put(self, value):
         raise NotImplementedError()
 
@@ -23,9 +27,53 @@ class Queue(asyncio.Queue, AbstractQueue):
         AbstractQueue.__init__(self, config, context=context, loop=loop)
         asyncio.Queue.__init__(self, maxsize=maxsize, loop=loop)
 
+    def __len__(self):
+        return len(self._queue)
 
-class PriorityQueue(asyncio.PriorityQueue, AbstractQueue):
+
+class PriorityQueue(asyncio.PriorityQueue, Queue):
     def __init__(self, config, *, context=None, loop=None):
         maxsize = config.get('maxsize', 0)
         AbstractQueue.__init__(self, config, context=context, loop=loop)
         asyncio.Queue.__init__(self, maxsize=maxsize, loop=loop)
+
+
+class ScoreQueueMixin:
+    """
+    config:
+        default_score: default value score
+    """
+
+    def init(self):
+        self._default_score = self.config.get(
+            'default_score', self.default_score)
+        if isinstance(self._default_score, str):
+            self._default_score = import_name(self._default_score)
+        return super().init()
+
+    def put(self, value, score=None):
+        if score is None:
+            if callable(self._default_score):
+                score = self._default_score()
+        v = score, value
+        return super().put(v)
+
+    async def get(self, score=False):
+        s, val = await super().get()
+        if score:
+            return val, s
+        else:
+            return val
+
+
+def score_queue(default_score=None):
+    return lambda klass: type(
+        klass.__name__,
+        (ScoreQueueMixin, klass),
+        {'default_score': default_score, 'super': klass}
+    )
+
+
+@score_queue()
+class ScoreQueue(PriorityQueue):
+    pass
