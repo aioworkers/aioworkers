e3e75dcad1c79d6682cb935a94d686dcf00b5420
Alexander Malev <yttrium@somedev.ru>
2017-03-10 03:03:55 +0300

    tests queue.base

---
+++ tests/test_queue.py
@@ -0,0 +1,36 @@
+from aioworkers.core.config import MergeDict
+from aioworkers.queue.base import Queue, ScoreQueue
+
+
+async def test_queue(loop):
+    q = Queue({}, loop=loop)
+    await q.init()
+    await q.put(2)
+    assert 2 == await q.get()
+
+
+async def test_score_base(loop):
+    q = ScoreQueue({}, loop=loop)
+    await q.init()
+    await q.put(2, 3)
+    assert 2 == await q.get()
+    await q.put(2, 3)
+    assert (2, 3) == await q.get(score=True)
+    assert not q
+
+
+async def test_score_time(loop):
+    q = ScoreQueue(MergeDict(
+        default_score='time.time',
+    ), loop=loop)
+    await q.init()
+    await q.put(2)
+    assert 2 == await q.get()
+    await q.put(3)
+    await q.put(4)
+    a1, t1 = await q.get(score=True)
+    a2, t2 = await q.get(score=True)
+    assert a1 == 3
+    assert a2 == 4
+    assert t1 < t2
+    assert not q
