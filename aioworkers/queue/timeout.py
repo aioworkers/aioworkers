253896be3dd1dd71dace8e0b43967c519d8738ec
Alexander Malev <yttrium@somedev.ru>
2017-03-11 00:30:03 +0300

    ScoreQueue interface implements in TimestampQueue

--- aioworkers/queue/timeout.py
+++ aioworkers/queue/timeout.py
@@ -2,39 +2,50 @@ import asyncio
 import heapq
 import time
 
-from .base import AbstractQueue
+from .base import AbstractQueue, ScoreQueueMixin
 
 
-class TimestampQueue(AbstractQueue):
-    async def init(self):
+class TimestampQueue(ScoreQueueMixin, AbstractQueue):
+    default_score = 'time.time'
+
+    def init(self):
         self._future = None
         self._queue = []
         self._waiters = []
+        self.context.on_stop.append(self.stop)
+        return super().init()
 
     async def stop(self):
         if self._future and not self._future.done():
             self._future.cancel()
-        for i in self._waiters:
+        for s, i in self._waiters:
             i.cancel()
+        self._waiters = []
 
     def __len__(self):
         return len(self._queue)
 
-    def get(self):
+    def get(self, score=False):
         waiter = self.loop.create_future()
         if self._queue:
             timestamp, value = heapq.nsmallest(1, self._queue)[0]
             if time.time() >= timestamp:
                 ts, value = heapq.heappop(self._queue)
-                waiter.set_result(value)
+                if score:
+                    waiter.set_result((value, ts))
+                else:
+                    waiter.set_result(value)
                 return waiter
             if not self._future or self._future.done():
                 self._future = self.loop.create_task(self._timer())
-        self._waiters.append(waiter)
+        self._waiters.append((score, waiter))
         return waiter
 
-    async def put(self, value):
-        heapq.heappush(self._queue, value)
+    async def put(self, value, score=None):
+        if score is None:
+            if callable(self._default_score):
+                score = self._default_score()
+        heapq.heappush(self._queue, (score, value))
         if self._future and not self._future.done():
             self._future.cancel()
         self._future = self.loop.create_task(self._timer())
@@ -46,23 +57,30 @@ class TimestampQueue(AbstractQueue):
         ts = heapq.nsmallest(1, self._queue)[0][0]
         t = ts - time.time()
         if t < 0:
-            f = self._waiters.pop(0)
-            f.set_result(heapq.heappop(self._queue)[1])
+            score, f = self._waiters.pop(0)
+            ts, val = heapq.heappop(self._queue)
+            if score:
+                f.set_result((val, ts))
+            else:
+                f.set_result(val)
         else:
             await asyncio.sleep(t, loop=self.loop)
             self._future = self.loop.create_task(self._timer())
 
 
 class UniqueQueue(TimestampQueue):
-    async def put(self, value):
-        ts, val = value
+    async def put(self, value, score=None):
+        if score is None:
+            if callable(self._default_score):
+                score = self._default_score()
+        item = score, value
         for i, (t, v) in enumerate(self._queue):
-            if v == val:
-                self._queue[i] = value
+            if v == value:
+                self._queue[i] = item
                 heapq.heapify(self._queue)
                 break
         else:
-            heapq.heappush(self._queue, value)
+            heapq.heappush(self._queue, item)
         if self._future and not self._future.done():
             self._future.cancel()
         self._future = self.loop.create_task(self._timer())
--- tests/test_queue_timestamp.py
+++ tests/test_queue_timestamp.py
@@ -4,16 +4,16 @@ import time
 from aioworkers.queue.timeout import TimestampQueue, UniqueQueue
 
 
-async def test_put(loop):
-    q = TimestampQueue({}, loop=loop)
+async def test_put(loop, mocker):
+    q = TimestampQueue({}, context=mocker.MagicMock(), loop=loop)
     await q.init()
-    await q.put((1, 1))
+    await q.put(1, 1)
     assert 1 == await q.get()
 
     t = time.time()
-    await q.put((t + 0.2, 2))
-    await q.put((t + 0.3, 3))
-    await q.put((t + 0.1, 1))
+    await q.put(2, t + 0.2)
+    await q.put(3, t + 0.3)
+    await q.put(1, t + 0.1)
     await asyncio.sleep(0.2, loop=loop)
     assert 1 == await q.get()
     assert 2 == await q.get()
@@ -21,23 +21,52 @@ async def test_put(loop):
     assert not q
 
 
-async def test_put_unique(loop):
-    q = UniqueQueue({}, loop=loop)
+async def test_put_unique(loop, mocker):
+    q = UniqueQueue({}, context=mocker.MagicMock(), loop=loop)
     await q.init()
-    await q.put((1, 1))
-    await q.put((2, 1))
-    await q.put((3, 1))
-    await q.put((3, 2))
-    await q.put((2, 2))
-    await q.put((1, 2))
+    await q.put(1, 1)
+    await q.put(1, 2)
+    await q.put(1, 3)
+    await q.put(2, 3)
+    await q.put(2, 2)
+    await q.put(2, 1)
     assert 2 == await q.get()
     assert 1 == await q.get()
     assert not q
 
 
-async def test_stop(loop):
-    q = TimestampQueue({}, loop=loop)
+async def test_timestamp_score(loop, mocker):
+    q = TimestampQueue({}, context=mocker.MagicMock(), loop=loop)
+    await q.init()
+    await q.put(1)
+    a, ts = await q.get(score=True)
+    assert a == 1
+    assert ts < time.time()
+
+
+async def test_timestamp_await(loop, mocker):
+    q = TimestampQueue({}, context=mocker.MagicMock(), loop=loop)
+    await q.init()
+    f = q.get(score=True)
+    t0 = time.time()
+    await q.put(1)
+    a, ts = await f
+    assert a == 1
+    assert t0 < ts < time.time()
+
+
+async def test_uniq_score(loop, mocker):
+    q = UniqueQueue({}, context=mocker.MagicMock(), loop=loop)
+    await q.init()
+    await q.put(1)
+    a, ts = await q.get(score=True)
+    assert a == 1
+    assert ts < time.time()
+
+
+async def test_stop(loop, mocker):
+    q = TimestampQueue({}, context=mocker.MagicMock(), loop=loop)
     await q.init()
     q.get()
-    await q.put((1, 1))
+    await q.put(1, 1)
     await q.stop()
