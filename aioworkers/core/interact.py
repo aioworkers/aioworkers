645cab2009a3d89e6baf49672c1e42f1c33ec2ad
Alexander Malev <yttrium@somedev.ru>
2017-03-16 23:03:58 +0300

    interact await function

--- aioworkers/core/interact.py
+++ aioworkers/core/interact.py
@@ -1,10 +1,29 @@
+import concurrent.futures
+from functools import partial
 from threading import Thread
+
+
+def _await(coro, context):
+    f = concurrent.futures.Future()
+    async def wrap(coro):
+        try:
+            result = await coro
+        except Exception as e:
+            f.set_exception(e)
+        else:
+            f.set_result(result)
+    context.loop.call_soon_threadsafe(
+        context.loop.create_task, wrap(coro)
+    )
+    return f.result()
 
 
 class Shell(Thread):
     def run(self):
         from IPython import embed
+
         context, = self._args
+        await = partial(_await, context=context)
         embed()
 
 
