8876e1fbaa3e4e7eae7cdc65c07c70a75d64c6ca
Alexander Malev <yttrium@somedev.ru>
2017-03-17 00:51:33 +0300

    context access optimization

--- aioworkers/core/loader.py
+++ aioworkers/core/loader.py
@@ -17,9 +17,9 @@ async def load_entities(conf, context=None, loop=None, entities=None, path=()):
         cls = utils.import_name(conf['app.cls'])
         app = await cls.factory(
             config=conf, context=context, loop=loop)
-        conf['app'] = app
 
         if context is not None:
+            context.app = app
             app.on_startup.append(lambda x: context.start())
             app.on_shutdown.append(lambda x: context.stop())
 
@@ -32,12 +32,13 @@ async def load_entities(conf, context=None, loop=None, entities=None, path=()):
         elif k in ('logging', 'app'):
             pass
         elif 'cls' in v:
+            p = path + (k,)
+            str_path = '.'.join(p)
             if 'name' not in v:
-                v['name'] = '.'.join(path + (k,))
-            cls = utils.import_name(v['cls'])
-            logger.debug('Imported "{}" as {}'.format(v['cls'], cls))
-            conf[k] = cls(v, context=context, loop=loop)
-            entities[path + (k,)] = conf[k]
+                v['name'] = str_path
+            entity = context[v]
+            context[str_path] = entity
+            entities[p] = entity
         else:
             conf[k] = await load_entities(
                 conf[k],
