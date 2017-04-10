185230919b268bee46ec5a235c7d6357a6b0fb19
Alexander Malev <yttrium@somedev.ru>
2017-04-10 17:38:52 +0300

    0.4.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requirements = [
 
 setup(
     name='aioworkers',
-    version='0.4.2',
+    version='0.4.3',
     description="Easy configurable workers based on asyncio",
     long_description=readme + '\n\n' + history,
     author="Alexander Malev",
