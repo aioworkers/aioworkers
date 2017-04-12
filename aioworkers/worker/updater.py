589a6928ea09b91801286e90c8e849ed54789666
Alexander Malev <yttrium@somedev.ru>
2017-04-12 11:26:10 +0300

    base updater

---
+++ aioworkers/worker/updater.py
@@ -0,0 +1,75 @@
+import atexit
+import os
+import sys
+import pkg_resources
+
+from aioworkers.worker.subprocess import Subprocess
+
+
+class BaseUpdater(Subprocess):
+    def init(self):
+        c = self.config
+        c.autorun = c.get('autorun', True)
+        c.persist = c.get('persist', True)
+        c.stderr = c.get('stderr', None)
+        c.stdout = c.get('stdout', None)
+        c.crontab = c.get('crontab', '0 * * * *')
+        return super().init()
+
+    async def can_restart(self):
+        return True
+
+    async def wait_restart(self):
+        pass
+
+    async def restart(self):
+        atexit.register(
+            os.execl, sys.executable,
+            sys.executable, *sys.argv)
+        self.loop.stop()
+
+    async def can_update(self):
+        return True
+
+    async def update(self):
+        await self.run_cmd()
+
+    async def run(self, value=None):
+        if not await self.can_update():
+            return
+        await self.update()
+        if await self.can_restart():
+            await self.wait_restart()
+            await self.restart()
+
+
+class PipUpdater(BaseUpdater):
+    def init(self):
+        c = self.config
+        c.cmd = c.get('cmd', [
+            sys.executable,
+            '-m', 'pip', 'install', '-U',
+        ])
+        self.v = self.version(c.package.name)
+        return super().init()
+
+    async def can_update(self):
+        # TODO parse https://pypi.python.org/pypi?:action=doap&name={package.name}
+        return True
+
+    @classmethod
+    def version(cls, package):
+        try:
+            return pkg_resources.get_distribution(package).version
+        except pkg_resources.DistributionNotFound:
+            pass
+
+    async def update(self):
+        package = self.config.package
+        val = package.get('link', package.name)
+        await self.run_cmd(val)
+
+    async def can_restart(self):
+        if not await super().can_restart():
+            return False
+        return self.v != self.version(self.config.package.name)
---
+++ tests/test_worker_updater.py
@@ -0,0 +1,13 @@
+from aioworkers.core.config import MergeDict
+from aioworkers.core.context import Context
+from aioworkers.worker.updater import BaseUpdater
+
+
+async def test_1(loop):
+    config = MergeDict(
+        name='',
+        autorun=False,
+    )
+    context = Context(config, loop=loop)
+    worker = BaseUpdater(config, context=context, loop=loop)
+    await worker.init()
