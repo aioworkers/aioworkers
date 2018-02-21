c30a39d8354848ee5055fb745c968432ea7061fc
Alexander Malev <yttrium@somedev.ru>
2018-02-21 03:47:30 +0300

    queue.proxy

---
+++ aioworkers/queue/proxy.py
@@ -0,0 +1,63 @@
+import sys
+import asyncio
+
+from ..core.base import ExecutorEntity
+from ..core.formatter import FormattedEntity
+from .base import AbstractQueue
+
+
+class ProxyQueue(ExecutorEntity, AbstractQueue):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._lock = None
+
+    async def init(self):
+        lock = asyncio.Lock(loop=self._loop)
+        await lock.acquire()
+        self._lock = lock
+
+    def set_queue(self, queue):
+        self._queue = queue
+        self._lock.release()
+
+    async def get(self):
+        async with self._lock:
+            return await self.run_in_executor(self._queue.get)
+
+    async def put(self, value):
+        async with self._lock:
+            return await self.run_in_executor(self._queue.put, value)
+
+
+class PipeLineQueue(FormattedEntity, ExecutorEntity, AbstractQueue):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._read_lock = asyncio.Lock(loop=self._loop)
+        self._write_lock = asyncio.Lock(loop=self._loop)
+        self.set_reader(sys.stdin.buffer)
+        self.set_writer(sys.stdout.buffer)
+        self._timeout = self.config.get('timeout')
+
+    def set_reader(self, reader):
+        self._reader = reader
+
+    def set_writer(self, writer):
+        self._writer = writer
+
+    async def get(self):
+        async with self._read_lock:
+            while True:
+                v = await self.run_in_executor(
+                    self._reader.readline)
+                if v:
+                    break
+                elif not self._timeout:
+                    return
+                await asyncio.sleep(self._timeout)
+        return self.decode(v)
+
+    async def put(self, value):
+        value = self.encode(value)
+        async with self._write_lock:
+            return await self.run_in_executor(
+                self._writer.write, value)
---
+++ tests/test_queue_proxy.py
@@ -0,0 +1,34 @@
+from queue import Queue
+
+import io
+
+from aioworkers import utils
+from aioworkers.core.context import Context
+from aioworkers.queue import proxy
+
+
+async def test_q(loop):
+    c = dict(
+        cls=utils.import_uri(proxy.ProxyQueue),
+    )
+
+    async with Context(dict(q=c), loop=loop) as ctx:
+        ctx.q.set_queue(Queue())
+        await ctx.q.put(1)
+        assert 1 == await ctx.q.get()
+
+
+async def test_plq(loop):
+    c = dict(
+        cls=utils.import_uri(proxy.PipeLineQueue),
+        format='newline:str',
+    )
+
+    async with Context(dict(q=c), loop=loop) as ctx:
+        fin = io.BytesIO(b'123\n')
+        fout = io.BytesIO()
+        ctx.q.set_reader(fin)
+        ctx.q.set_writer(fout)
+        assert '123' == await ctx.q.get()
+        await ctx.q.put('1')
+        assert b'1\n' == fout.getvalue()
