e9c16e79af38d2bf529e963ae0b7e70a66053478
Alexander Malev <yttrium@somedev.ru>
2018-05-07 12:45:49 +0300

    drop deprecated modules & type URL

--- aioworkers/http.py
+++ aioworkers/http.py
@@ -1,21 +1,5 @@
-import asyncio
 
-from aiohttp import web
-
-from .app import BaseApplication
-
-
-class Application(BaseApplication, web.Application):
-    """DEPRECATED class"""
-    def __init__(self, config, *, context, **kwargs):
-        if kwargs.get('loop') is None:
-            kwargs['loop'] = asyncio.get_event_loop()
-        web.Application.__init__(self, **kwargs)
-        BaseApplication.__init__(
-            self, config=config, context=context, **kwargs)
-
-    def run_forever(self, port=None, host=None, **kwargs):
-        gconf = self.context.config.http
-        kwargs['host'] = host or gconf.host
-        kwargs['port'] = port or gconf.port
-        web.run_app(self, **kwargs)
+try:
+    from yarl import URL
+except ImportError:
+    URL = type('URL', (str,), {})
