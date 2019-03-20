9af8590cb236384d20d536697f15d9bf63b2701f
Alexander Malev <yttrium@somedev.ru>
2019-03-21 02:35:14 +0300

    use pytest-aioworkers

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,5 @@
 import pytest
 
-from aioworkers.core.config import Config
-from aioworkers.core.context import Context
-
-
-@pytest.fixture
-def config_yaml():
-    return ''
-
-
-@pytest.fixture
-def config(config_yaml):
-    c = Config()
-    if config_yaml:
-        import yaml
-        loader = getattr(yaml, 'CLoader', yaml.Loader)
-        c.update(yaml.load(config_yaml, loader))
-    return c
-
-
-@pytest.fixture
-def context(loop, config):
-    with Context(config, loop=loop) as ctx:
-        yield ctx
-
 
 @pytest.fixture
 def make_coro():
