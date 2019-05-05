dc60c4b2d06a6dbaa1cc5ab1c97ea3509ac60be6
Alexander Malev <yttrium@somedev.ru>
2019-05-06 00:44:48 +0300

    sender.proxy

---
+++ aioworkers/net/sender/proxy.py
@@ -0,0 +1,25 @@
+from aioworkers.core.base import AbstractWriter, LoggingEntity
+from aioworkers.net.sender import AbstractSender
+from aioworkers.worker.base import Worker as BaseWorker
+
+
+class Facade(LoggingEntity, AbstractSender):
+    _queue = None  # type: AbstractWriter
+
+    async def init(self):
+        await super().init()
+        self._queue = self.context[self.config.queue]
+
+    async def send_message(self, msg):
+        await self._queue.put(msg)
+
+
+class Worker(BaseWorker):
+    _sender = None  # type: AbstractSender
+
+    async def init(self):
+        await super().init()
+        self._sender = self.context[self.config.sender]
+
+    async def run(self, value=None):
+        await self._sender.send(value)
---
+++ tests/test_net_sender_proxy.py
@@ -0,0 +1,37 @@
+import pytest
+
+
+@pytest.fixture
+def config_yaml():
+    return """
+    local_sender:
+        cls: aioworkers.net.sender.proxy.Facade
+        queue: queue1
+
+    queue1:
+        cls: aioworkers.queue.base.Queue
+
+    worker:
+        cls: aioworkers.net.sender.proxy.Worker
+        autorun: true
+        input: queue1
+        sender: remote_sender
+
+    remote_sender:
+        cls: aioworkers.net.sender.proxy.Facade
+        queue: queue2
+
+    queue2:
+        cls: aioworkers.queue.base.Queue
+    """
+
+
+async def test_proxy_chains(context):
+    await context.local_sender.send(
+        to='example@example.com',
+        subject='test',
+        content='text',
+        html='<b>text</b>',
+    )
+    msg = await context.queue2.get()
+    assert msg['subject'] == 'test'
