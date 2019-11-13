40927a340a5d3a434b9b70c2ad0bfb9568e990ec
Alexander Malev <yttrium@somedev.ru>
2019-11-13 03:13:26 +0300

    AbstractHttpStorage

--- aioworkers/storage/http.py
+++ aioworkers/storage/http.py
@@ -1,18 +1,30 @@
-import asyncio
-import json
 import logging
+from abc import abstractmethod
 from collections import Mapping, Sequence
 
-from aiohttp import ClientOSError, client
-
+from ..core.base import ExecutorEntity, LoggingEntity
+from ..core.formatter import FormattedEntity
 from ..http import URL
+from ..net.web.client import Session
 from . import StorageError, base
 
 
-class RoStorage(base.AbstractStorageReadOnly):
+class StorageConnectionError(StorageError):
+    pass
+
+
+class StorageDataFormatError(StorageError):
+    pass
+
+
+class AbstractHttpStorage(
+    FormattedEntity, LoggingEntity, base.AbstractStorageReadOnly,
+):
     """ ReadOnly storage over http GET
     config:
-        semaphore: int
+        conn_limit: int = 1
+        conn_timeout: float
+        read_timeout: float
         allow_hosts: list
         return_status: bool, method get returns tuple (CODE, VALUE)
         prefix: url prefix
@@ -20,13 +32,15 @@ class RoStorage(base.AbstractStorageReadOnly):
         template: url template
         format: [json|str|bytes], default json
     """
+    def __init__(self, *args, **kwargs):
+        self.session = None
+        super().__init__(*args, **kwargs)
+
     async def init(self):
         self._prefix = self.config.get('prefix')
         self._template = self.config.get('template')
         if self._prefix:
             self._prefix = URL(self._prefix)
-        self._semaphore = asyncio.Semaphore(
-            self.config.get('semaphore', 20), loop=self.loop)
         self._allow_hosts = self.config.get('allow_hosts')
         self._format = self.config.get('format', 'json')
         self._return_status = self.config.get('return_status', False)
@@ -34,22 +48,25 @@ class RoStorage(base.AbstractStorageReadOnly):
         headers = self.config.get('headers')
         self.session_params = {}
         if headers:
-            self.session_params['headers'] = dict(headers)
+            self.session_params['headers'] = headers
         for param in ('conn_timeout', 'read_timeout'):
             if param in self.config:
                 self.session_params[param] = self.config[param]
-        self.reset_session()
+        self.session = await self.session_factory(**self.session_params)
         self.context.on_stop.append(self.stop)
 
-    def reset_session(self, **kwargs):
-        session = getattr(self, 'session', None)
-        if session:
-            asyncio.ensure_future(session.close(), loop=self.loop)
+    @abstractmethod
+    async def session_factory(self, **kwargs):
+        pass
+
+    async def reset_session(self, **kwargs):
+        if self.session is not None:
+            await self.session.close()
         if kwargs:
             kwargs = {**self.session_params, **kwargs}
         else:
             kwargs = self.session_params
-        self.session = client.ClientSession(loop=self.loop, **kwargs)
+        self.session = await self.session_factory(**kwargs)
 
     async def stop(self):
         await self.session.close()
@@ -71,31 +88,14 @@ class RoStorage(base.AbstractStorageReadOnly):
             raise KeyError(key)
         return url
 
-    async def _request(self, url, *, method='get', **kwargs):
-        async with self._semaphore:
-            coro = getattr(self.session, method)
-            async with coro(url, **kwargs) as response:
-                if self._format == 'json' and 'json' in response.content_type:
-                    return response.status, await response.json()
-                elif self._format == 'str':
-                    return response.status, await response.text()
-                else:
-                    return response.status, await response.read()
-
     async def request(self, url, **kwargs):
-        try:
-            status, data = await self._request(url, **kwargs)
-        except ClientOSError as e:
-            raise StorageError('URL %s: %s' % (url, e)) from e
-
+        async with self.session.request(url, **kwargs) as response:
+            data = await response.read()
+        result = self.decode(data)
         if self._return_status:
-            return status, data
-
-        if status == 404:
-            data = None
-        elif status >= 400:
-            raise StorageError('URL %s: %s' % (url, status))
-        return data
+            return response.status, result
+        else:
+            return result
 
     def get(self, key):
         url = self.raw_key(key)
@@ -110,29 +110,38 @@ class RoStorage(base.AbstractStorageReadOnly):
         if not isinstance(storage_dest, FileSystemStorage):
             return super().copy(key_source, storage_dest, key_dest)
         url = self.raw_key(key_source)
-        logger = self.context.logger
-        async with self._semaphore:
-            async with self.session.get(url) as response:
-                if response.status == 404:
-                    return
-                elif response.status >= 400:
-                    if logger.getEffectiveLevel() == logging.DEBUG:
-                        logger.debug(
-                            'HttpStorage request to %s '
-                            'returned code %s:\n%s' % (
-                                url, response.status,
-                                (await response.read()).decode()))
-                    return
-                async with storage_dest.raw_key(key_dest).open('wb') as f:
-                    async for chunk in response.content.iter_any():
-                        await f.write(chunk)
-                    return True
+        async with self.session(url, method='get') as response:
+            if response.status == 404:
+                return
+            elif response.status >= 400:
+                if self.logger.getEffectiveLevel() == logging.DEBUG:
+                    self.logger.debug(
+                        'HttpStorage request to %s '
+                        'returned code %s:\n%s' % (
+                            url, response.status,
+                            (await response.read()).decode()))
+                return
+            async with storage_dest.raw_key(key_dest).open('wb') as f:
+                async for chunk in response.content.iter_any():
+                    await f.write(chunk)
+                return True
+
+
+class RoStorage(ExecutorEntity, AbstractHttpStorage):
+    async def session_factory(self, **kwargs):
+        return Session.from_entity(self, **kwargs)
+
+    async def request(self, url, **kwargs):
+        try:
+            return await super().request(url, **kwargs)
+        except Exception as e:
+            raise StorageError() from e
 
 
 class Storage(RoStorage, base.AbstractStorageWriteOnly):
     """ RW storage over http
     config:
-        semaphore: int
+        conn_limit: int
         allow_hosts: list
         return_status: bool, method get returns tuple (CODE, VALUE)
         prefix: url prefix
@@ -140,21 +149,14 @@ class Storage(RoStorage, base.AbstractStorageWriteOnly):
         headers: Mapping or None
         format: [json|str|bytes], default json
         set: [post|put|patch], default post
-        dumps: str, path in context to dumps
     """
 
     def set(self, key, value):
         url = self.raw_key(key)
-        if self._format == 'json':
-            if self.config.get('dumps'):
-                dumps = self.context[self.config.dumps]
-            else:
-                dumps = json.dumps
-            data = dumps(value)
-            headers = {'content-type': 'application/json'}
-        else:
-            data = value
-            headers = {}
+        data = self.encode(value)
+        headers = {}
+        if self._formatter.mimetypes:
+            headers['Content-Type'] = self._formatter.mimetypes[0]
 
         return self.request(
             url, method=self.config.get('set', 'post'),
