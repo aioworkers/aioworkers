37ceeee748435bd4503faf62f0634ea38c0c9291
Alexander Malev <yttrium@somedev.ru>
2019-12-08 22:20:32 +0300

    cache for loaded plugins

--- aioworkers/core/plugin.py
+++ aioworkers/core/plugin.py
@@ -1,38 +1,47 @@
+import functools
 import logging
 import sys
 from pathlib import Path
+from typing import Dict, Iterable, Optional
 
 from . import config, formatter
 
 logger = logging.getLogger(__name__)
 
 
-def load_plugin(module: str, force=False):
-    if force:
+def load_plugin(
+    module: str, force: bool = False, *, cache: Dict = {},
+) -> Optional['Plugin']:
+    if module in cache:
+        return cache[module]
+    elif force:
         pass
     elif module in sys.modules:
-        return
+        return None
     try:
         m = __import__(module, fromlist=['plugin'])
     except ImportError:
-        return
+        return None
     plugin = getattr(m, 'plugin', m)
     if callable(plugin):
         plugin = plugin()
-    if isinstance(plugin, Plugin):
-        return plugin
-    return ProxyPlugin(plugin)
+    if not isinstance(plugin, Plugin):
+        plugin = ProxyPlugin(plugin)
+    cache[module] = plugin
+    return plugin
 
 
-def get_names():
-    for path in sys.path:
-        path = Path(path)
+@functools.lru_cache(None)
+def get_names() -> Iterable[str]:
+    for s in sys.path:
+        path = Path(s)
         if not path.is_dir():
             continue
         for i in path.glob('aioworkers*'):
             yield i.with_suffix('').name
 
 
+@functools.lru_cache(None)
 def search_plugins(*modules, force=False):
     result = []
     if not modules:
@@ -71,8 +80,8 @@ class ProxyPlugin(Plugin):
     def __init__(self, original):
         self._original = original
         for i in (
-                'formatters', 'config_loaders', 'configs',
-                'get_config', 'add_arguments',
+            'formatters', 'config_loaders', 'configs',
+            'get_config', 'add_arguments',
         ):
             v = getattr(original, i, None)
             if v:
