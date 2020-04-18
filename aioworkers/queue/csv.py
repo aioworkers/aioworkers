64fb0375a7cf75cc61f64a3d1168c73108758d8b
Alexander Malev <yttrium@somedev.ru>
2020-04-18 23:28:18 +0300

    cleanup for DictReader

--- aioworkers/queue/csv.py
+++ aioworkers/queue/csv.py
@@ -6,12 +6,18 @@ from ..core.base import AbstractReader
 
 class DictReader(AbstractReader):
     async def init(self):
-        self._reader = csv.DictReader(open(self.config.file))
         self._executor = None  # TODO
         self._lock = asyncio.Lock(loop=self.loop)
+        self._file = open(self.config.file)
+        self._reader = csv.DictReader(self._file)
+        self.context.on_cleanup.append(self.cleanup)
 
     async def get(self):
         # will not raise an exception StopIteration TODO ?
         async with self._lock:
             return await self.loop.run_in_executor(
-                self._executor, next, self._reader)
+                self._executor, next, self._reader,
+            )
+
+    def cleanup(self):
+        self._file.close()
