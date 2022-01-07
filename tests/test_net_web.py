f9d0134725d501bed47133c4baa8d6c7378de4a9
Alexander Malev <yttrium@somedev.ru>
2022-01-07 19:47:00 +0300

    drop sleep from test_web_server

--- tests/test_net_web.py
+++ tests/test_net_web.py
@@ -44,7 +44,6 @@ async def handler_post(request, context):
 
 @pytest.mark.timeout(5)
 async def test_web_server(context):
-    await asyncio.sleep(2)
     url = context.http.url
     assert 1 == await context.storage.get(url / 'api')
     assert 'asdf' == await context.storage.get(url / 'api/str')
