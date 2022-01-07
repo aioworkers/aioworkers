3b4ea6f1b4a4056299e18680ba40832ecaa6610f
Alexander Malev <yttrium@somedev.ru>
2022-01-08 02:38:22 +0300

    WebServer cover up

--- tests/test_net_web.py
+++ tests/test_net_web.py
@@ -13,7 +13,7 @@ def aioworkers(aioworkers):
 def config_yaml(unused_port):
     return """
     http.port: {port}
-    web.resources:
+    app.resources:
         /api:
             get: .data
         /api/str:
