fee3507a817bafaf58f48b096d718656c3d0ed6a
Alexander Malev <yttrium@somedev.ru>
2022-01-22 21:13:05 +0300

    improve test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,16 @@
+import os
+from unittest import mock
+
 from aioworkers import utils
 
 
-def test_random_seed():
+def test_urandom_seed():
     utils.random_seed()
+
+
+def test_random_seed():
+    def not_implemented_urandom(*args):
+        raise NotImplementedError
+
+    with mock.patch.object(os, "urandom", not_implemented_urandom):
+        utils.random_seed()
