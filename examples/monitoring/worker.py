eb03de241f3d47173381ee22f85b5cdf5d9c1fb4
Ross Nomann <rossnomann@protonmail.com>
2022-06-06 11:14:30 +0200

    Fix flake8 issues in examples

--- examples/monitoring/worker.py
+++ examples/monitoring/worker.py
@@ -11,8 +11,8 @@ GRAPHITE_HOST = getenv('GRAPHITE_HOST', 'localhost')
 async def run(worker, *args, **kwargs):
     value = random.randrange(10)
     try:
-    	connection = await connect(GRAPHITE_HOST, 2003, PlaintextProtocol(), loop=worker.loop)
-    	await connection.send('workers.worker', value, time.time())
-    	await connection.close()
-    except Exception as e:
-    	worker.logger.error('Cannot connect to graphite')
+        connection = await connect(GRAPHITE_HOST, 2003, PlaintextProtocol(), loop=worker.loop)
+        await connection.send('workers.worker', value, time.time())
+        await connection.close()
+    except Exception:
+        worker.logger.error('Cannot connect to graphite')
