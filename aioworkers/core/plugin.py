bc06ba99eac8609b5b09748dfc7a7b21731b75a3
Alexander Malev <aamalev@users.noreply.github.com>
2023-06-15 00:31:35 +0300

    Load plugins from entry_points (#165)
    * Load plugins from entry_points
    
    * search_plugins with entry_points

--- aioworkers/core/plugin.py
+++ aioworkers/core/plugin.py
@@ -1,12 +1,28 @@
 import functools
 import logging
 import sys
+from dataclasses import dataclass
 from pathlib import Path, PurePath
-from typing import Dict, Iterable, Optional, Sequence, Union
+from typing import Dict, Iterable, Mapping, Optional, Sequence, Union
 
 from . import config, formatter
 
 logger = logging.getLogger(__name__)
+
+if sys.version_info < (3, 8):  # no cov
+    from pkg_resources import EntryPoint, iter_entry_points
+else:
+    from importlib.metadata import EntryPoint, entry_points
+
+    def iter_entry_points(group: str, name: Optional[str] = None):
+        assert not name
+        eps = entry_points()
+        if sys.version_info < (3, 10):
+            group_eps = eps.get(group, ())
+        else:  # no cov
+            group_eps = eps.select(group=group)
+        for entry_point in group_eps:
+            yield entry_point
 
 
 def load_plugin(
@@ -21,39 +37,46 @@ def load_plugin(
         pass
     elif module in sys.modules:
         return None
-    try:
-        m = __import__(module, fromlist=['plugin'])
-    except ImportError:
-        return None
-    plugin = getattr(m, 'plugin', m)
-    if callable(plugin):
-        plugin = plugin()
-    if not isinstance(plugin, Plugin):
-        plugin = ProxyPlugin(plugin)
+
+    loaders = get_plugin_loaders()
+    pl = loaders.get(module) or PluginLoader(module)
+    plugin = pl.load()
     cache[module] = plugin
     return plugin
 
 
 @functools.lru_cache(None)
-def get_names() -> Iterable[str]:
+def get_names(group="aioworkers") -> Iterable[str]:
+    dedup = set()
     for s in sys.path:
         path = Path(s)
         if not path.is_dir():
             continue
-        for i in path.glob('aioworkers*'):
-            yield i.with_suffix('').name
+        for i in path.glob(f"{group}_*"):
+            name = i.name
+            if name not in dedup and "-" not in name:
+                dedup.add(name)
+                yield name
 
 
 @functools.lru_cache(None)
 def search_plugins(*modules: str, force=False):
     result = []
+    pls = get_plugin_loaders()
     if not modules:
-        modules = tuple(get_names())
-    for name in modules:
-        plugin = load_plugin(name, force=force)
-        if plugin:
-            logger.info('Loaded plugin {} from {}'.format(plugin, name))
-            result.append(plugin)
+        modules = tuple(pls)
+    for module in modules:
+        if not force and module in sys.modules:
+            continue
+        pl = pls.get(module) or PluginLoader(module)
+        plugin = pl.load()
+        if not plugin:
+            continue
+        elif pl.entry_point is None:
+            logger.info("Loaded plugin %s", module)
+        else:
+            logger.info("Loaded plugin %s", pl.entry_point)
+        result.append(plugin)
     return result
 
 
@@ -93,15 +116,62 @@ class ProxyPlugin(Plugin):
             if v:
                 setattr(self, i, v)
 
+        p = None
         if hasattr(self._original, '__file__'):
             p = self._original.__file__
         elif hasattr(self._original, '__module__'):
             mod = sys.modules[self._original.__module__]
             p = mod.__file__
-        else:
-            p = None
         if not self.configs and p is not None:
             path = Path(p)
             if path.name == '__init__.py':
                 self.configs = tuple(path.parent.glob('plugin*'))
         super().__init__()
+
+
+@dataclass
+class PluginLoader:
+    module: str
+    entry_point: Optional[EntryPoint] = None
+
+    @classmethod
+    def from_entry_point(cls, ep: EntryPoint) -> "PluginLoader":
+        if sys.version_info < (3, 8):  # no cov for 3.8
+            module = ep.module_name
+        elif sys.version_info < (3, 9):
+            m = ep.pattern.match(ep.value)
+            assert m is not None, f"Not valid value of EntryPoint {ep.value}"
+            module = m.group("module")
+        else:  # no cov for 3.8
+            module = ep.module
+        return cls(module, ep)
+
+    def load(self) -> Optional[Plugin]:
+        if self.entry_point is None:
+            try:
+                m = __import__(self.module, fromlist=['plugin'])
+            except ImportError:
+                return None
+            else:
+                plugin = getattr(m, 'plugin', m)
+        else:
+            plugin = self.entry_point.load()
+        if callable(plugin):
+            plugin = plugin()
+        if not isinstance(plugin, Plugin):
+            plugin = ProxyPlugin(plugin)
+        return plugin
+
+
+@functools.lru_cache(None)
+def get_plugin_loaders(group="aioworkers") -> Mapping[str, PluginLoader]:
+    modules_with_ep = set()
+    result = {}
+    for ep in iter_entry_points(group):
+        pl = PluginLoader.from_entry_point(ep)
+        modules_with_ep.add(pl.module.split(".", 1)[0])
+        result[pl.module] = pl
+    for module in get_names(group):
+        if module not in modules_with_ep:
+            result[module] = PluginLoader(module)
+    return result
--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -3,7 +3,15 @@ import sys
 
 import pytest
 
-from aioworkers.core.plugin import ProxyPlugin, search_plugins
+from aioworkers.core import plugin as core_plugin
+from aioworkers.core.plugin import (
+    PluginLoader,
+    ProxyPlugin,
+    get_names,
+    get_plugin_loaders,
+    load_plugin,
+    search_plugins,
+)
 
 
 class plugin:
@@ -19,3 +27,51 @@ def test_proxy_plugin(name, mocker):
     assert p.get_config() == {}
     p.add_arguments(mocker.Mock())
     p.parse_known_args(args=[], namespace=argparse.Namespace())
+
+
+def test_get_names():
+    modules = get_names("pytest")
+    assert modules
+
+
+def test_plugin_loader():
+    pl = PluginLoader("datetime")
+    assert pl.load()
+    pl = PluginLoader("import-error-module")
+    assert not pl.load()
+
+
+def test_load_plugin(mocker):
+    pls = {__name__: PluginLoader(__name__)}
+    mocker.patch.object(core_plugin, "get_plugin_loaders", lambda *a: pls)
+    plugin = load_plugin(__name__)
+    assert not plugin
+    plugin = load_plugin(__name__, force=True)
+    assert plugin
+    plugin = load_plugin(__name__, force=True)
+    assert plugin
+
+
+def test_search_plugins(mocker):
+    plugins = search_plugins(__name__, "import-error")
+    assert len(plugins) == 0
+
+    pls = {__name__: PluginLoader(__name__)}
+    mocker.patch.object(core_plugin, "get_plugin_loaders", lambda *a: pls)
+    plugins = search_plugins(force=True)
+    assert len(plugins) == 1
+
+    for ep in core_plugin.iter_entry_points("pytest11"):
+        pl = PluginLoader.from_entry_point(ep)
+        pls[pl.module] = pl
+
+    assert search_plugins("pytest_aioworkers.plugin", force=True)
+
+
+def test_get_plugin_loaders(mocker):
+    mocker.patch.object(core_plugin, "get_names", lambda *a: [__name__])
+    loaders = get_plugin_loaders("pytest11")
+    assert "" not in loaders
+    assert __name__ in loaders
+    for k, v in loaders.items():
+        assert v.load()
